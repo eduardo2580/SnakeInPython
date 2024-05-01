b9f3aeebb9baccd4081638a0c33e8b8e3fad821f
Eric Santos <ericshantos@gmail.com>
2024-05-01 15:08:40 -0300

    build(src): Cria o pacote config_apple
    Pacote responsável por gerenciar o mecanismo das
    maçãs dentro do jogo.

---
+++ src/apple_config/__init__.py
@@ -0,0 +1,16 @@
+"""
+Configuration module for the Apple class.
+
+This module provides the Apple class, which represents an 
+apple object in the game. The apple is an item that the 
+player can collect for points.
+"""
+
+# Internal import
+from .config_apple import Apple 
+
+# Author
+__author__ = "Eduardo Benatti <https://github.com/eduardo2580/SnakeInPython>"  
+
+# Version
+__version__ = "1.1.0"
