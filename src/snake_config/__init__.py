5e6a7bfa7e9adf1f464eb66d884a3498c1fc349e
Eric Santos <ericshantos@gmail.com>
2024-05-01 15:14:47 -0300

    build(src): Cria o pacote config_snake
    Pacote responsável por gerenciar o mecanismo da cobra dentro do jogo.

---
+++ src/snake_config/__init__.py
@@ -0,0 +1,16 @@
+"""
+Configuration module for the Snake class.
+
+This module provides the Snake class, which represents the snake 
+entity in the Snake game. The Snake class handles the snake's 
+movement, collision detection, and drawing on the screen.
+"""
+
+# Internal import
+from .snake_config import Snake
+
+# Author
+__author__ = "Eduardo Benatti <https://github.com/eduardo2580/SnakeInPython>"  
+
+# Version
+__version__ = "1.1.0"
